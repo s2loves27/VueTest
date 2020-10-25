76dfb7966744c9af74e4ca198f6399525ec54109
jeon <s2loves27@naver.com>
2020-10-25 13:22:27 +0900

    temp

---
+++ Vuejs/vue01docs/aa.js
@@ -0,0 +1,73 @@
+//import axios from 
+;
+
+new Vue({
+    /* edtmc^2w */
+    el: "",
+    data: {
+
+    },
+    //template: ``,
+    //이벤트 핸들러
+    methods: {
+
+    },
+    components: {},
+    computed: {
+
+    },
+    //동기식
+    watch: {
+
+    },
+    mounted: function () {
+        console.log("mounted");
+    },
+    updated: function () {
+        console.log("updated");
+    },
+});
+
+//인스턴스는 객체(data)
+//컴포넌트는 함수(data)
+Vue.component("aa", {
+    /* pdtmc^2w */
+    props: [],
+    data: function () {
+        return {}
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    components: {},
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
+
+Vue.component("aa", {
+    /* pdtmc^2w */
+    props: [],
+    data: function () {
+        return {}
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    components: {},
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
